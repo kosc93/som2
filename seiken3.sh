52416c845c127d97523d974ab011ab8779a0dee7
patrick258745 <patrick.schmager@tu-dresden.de>
2018-04-27 18:34:59 +0200

    save state

---
+++ seiken3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "$1" = "play" ]; then
+    zsnes -zs 0 seiken3.smc &
+  elif [ "$1" = "save" ]; then
+    git add -A;
+    git commit -m "save state";
+    git push;
+    echo "Save state successfully pushed to github"
+  else
+    echo "Wrong usage! [./seiken3.sh <play/save>]"
+fi    
